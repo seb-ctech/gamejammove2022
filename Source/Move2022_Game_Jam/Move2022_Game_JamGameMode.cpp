71abec4c3c43549df059e616734c1396b9be0a6e
Seb W <thelayeredmind@gmail.com>
2022-11-13 13:47:02 +0100

    Remove C++ Logic for TopDown

--- Source/Move2022_Game_Jam/Move2022_Game_JamGameMode.cpp
+++ Source/Move2022_Game_Jam/Move2022_Game_JamGameMode.cpp
@@ -7,20 +7,5 @@
 
 AMove2022_Game_JamGameMode::AMove2022_Game_JamGameMode()
 {
-	// use our custom PlayerController class
-	PlayerControllerClass = AMove2022_Game_JamPlayerController::StaticClass();
 
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownCharacter"));
-	if (PlayerPawnBPClass.Class != nullptr)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
-
-	// set default controller to our Blueprinted controller
-	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownPlayerController"));
-	if(PlayerControllerBPClass.Class != NULL)
-	{
-		PlayerControllerClass = PlayerControllerBPClass.Class;
-	}
 }
